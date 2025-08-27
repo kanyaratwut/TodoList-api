ed08b8eb0b4e2adfe7b16773f56b029525037f5b
champagne <kanyarat.wut@gmail.com>
2025-08-27 16:23:19 +0700

    set up cors

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ connectDB();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({
-  origin: "*",
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"]
-}));
-
-app.options("*", cors());
+app.use(cors());
 
 readdirSync("./routers").map((c) => {
   console.log(c);
